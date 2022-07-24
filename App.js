f770f7f24155620b51bddf606817e1b32bef368f
joey-pang <jkkpang@gmail.com>
2022-07-24 01:18:47 +0100

    Login / Register System Created

--- App.js
+++ App.js
@@ -1,24 +1,59 @@
-import { StatusBar } from "expo-status-bar";
+import "react-native-gesture-handler";
 import { StyleSheet, Text, View } from "react-native";
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { decode, encode } from "base-64";
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
 
 import HomeScreen from "./src/screens/Home";
 import RewardScreen from "./src/screens/Reward";
 import SetTaskScreen from "./src/screens/SetTask";
 import TaskList from "./src/screens/TaskList";
+import LoginScreen from "./src/screens/Login";
+import RegisterScreen from "./src/screens/Register";
+import { firebase } from "./config";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [initializing, setInitializing] = useState(true);
+
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged((user) => {
+      setUser(user);
+      setInitializing(false);
+    });
+    return subscriber;
+  }, []);
+
+  if (initializing) return null; //Here you may use an Activity indicator
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Reward" component={RewardScreen} />
-        <Stack.Screen name="SetTask" component={SetTaskScreen} />
-        <Stack.Screen name="TaskList" component={TaskList} />
+      <Stack.Navigator initialRouteName="Login">
+        {user ? (
+          <>
+            <Stack.Screen name="Home">
+              {(props) => <HomeScreen {...props} extraData={user} />}
+            </Stack.Screen>
+            <Stack.Screen name="Reward" component={RewardScreen} />
+            <Stack.Screen name="SetTask" component={SetTaskScreen} />
+            <Stack.Screen name="TaskList" component={TaskList} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
