670f88705a3d34343e0fb4775ae5e9c3ddef4d2d
CheddarVEVO <Cheddarvevo@gmail.com>
2022-07-24 01:37:02 +0100

    homepage background completed

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { StyleSheet, Text, View } from "react-native";
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import {AppLoading} from "expo";
+import {useFonts, Heebo_100Thin, Heebo_300Light, Heebo_400Regular, Heebo_500Medium, Heebo_600SemiBold, Heebo_700Bold, Heebo_800ExtraBold, Heebo_900Black } from "@expo-google-fonts/heebo";
 import HomeScreen from "./src/screens/Home";
 import RewardScreen from "./src/screens/Reward";
 import SetTaskScreen from "./src/screens/SetTask";
@@ -14,7 +15,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Reward" component={RewardScreen} />
         <Stack.Screen name="SetTask" component={SetTaskScreen} />
@@ -22,7 +23,7 @@ export default function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,52 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, Image, View, Button } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CountDown from 'react-native-countdown-component';
 
 function HomeScreen({ navigation }) {
+  const [isPlaying, setIsPlaying] = React.useState(true)
   return (
     <View style={styles.container}>
       <View>
-        <Text>Home Screen</Text>
+        <Text style={styles.mainheader}>Dashboard</Text>
       </View>
+
+      <View style={styles.uicon}>
+      <Image source={require("../../assets/images/User.png")} />
+      </View>
+
+      <View style={styles.background}>
+        <View style={styles.background}/>
+      </View>
+
+      <View style={styles.background2}>
+      <View style={styles.background2}/>
+      </View>
+
+      <View style={{position: 'absolute', top: -445, left: -220, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={styles.countdown}>Countdown:</Text>
+      </View>
+
+      <View style={styles.box1}>
+      </View>
+
+      <View style={styles.timer}>
+      <CountDown
+        until={86400}
+        size={30}
+        onFinish={() => alert('Finished')}
+        digitStyle={{backgroundColor: '#FFFFFF'}}
+        digitTxtStyle={{color: '#000000'}}
+        separatorStyle={{color: '#000000'}}
+        timeToShow={['D', 'H', 'M', 'S']}
+        timeLabels={{d: 'Days', h: 'Hours', m: 'Minutes', s: 'Seconds'}}
+        timeLabelStyle={{color:'#2C62FF'}}
+      />
+      </View>
+
+      <View style={styles.navbar}>
       <Button title="Set Task" onPress={() => navigation.navigate("SetTask")} />
       <Button
         title="Task List"
@@ -17,6 +54,7 @@ function HomeScreen({ navigation }) {
       />
       <Button title="Reward" onPress={() => navigation.navigate("Reward")} />
       <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -24,10 +62,60 @@ function HomeScreen({ navigation }) {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  container: {   
+  marginTop: 0,
+  marginLeft: 0,
+  marginRight: 0,
   },
+  mainheader: {
+  marginTop: 70, 
+  marginLeft: 25,
+  fontSize: 45,
+  },
+  uicon: {
+    marginTop: -49,
+    marginLeft: 300,
+  },
+  background: {
+    width: 375,
+    height: 688,
+    backgroundColor: '#7699FF',
+    borderradius: 15,
+    transform: [{ rotate: '-6deg' }],
+    borderTopRightRadius: 15,
+    marginTop: 45,
+    marginLeft: -10,
+  },
+  background2: {
+    width: 374.85,
+    height: 688,
+    backgroundColor: '#2C62FF',
+    borderTopRightRadius: 15,
+    marginTop: 225,
+    position: "absolute",
+  },
+  countdown: {
+    fontSize: 18,
+    color: "#FFFFFF",
+    position: "absolute",
+    },
+  box1: {
+    width: 340,
+    height: 125,
+    backgroundColor: '#FFFFFF',
+    borderTopRightRadius: 15,
+    borderTopLeftRadius: 15,
+    borderBottomRightRadius: 15,
+    borderBottomLeftRadius: 15,
+    marginTop: 275,
+    marginLeft: 20,
+    position: "absolute",
+  },
+  timer: {
+    position: "absolute",
+    marginTop: 284,
+    marginLeft: 40,
+    transform: [{ scale: 1.1 }]
+  }
+
 });
